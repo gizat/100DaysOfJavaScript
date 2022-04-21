31cc3619532ead7ed25247ea5eeda182618d21e2
gizat <gizat.makhanov@gmail.com>
2022-04-21 17:52:49 +0600

    Initial setup

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,59 @@
+let country = "Kazakhstan";
+const continent = "Eurasia";
+let population = 18.75;
+const isIsland = false;
+const language = 'kazakh';
+
+// console.log(population / 2)
+
+// population++;
+// console.log(population);
+
+// isMoreThanFinland = population > 6;
+// console.log(isMoreThanFinland);
+
+// isLessThanAverage = population < 33;
+// console.log(isLessThanAverage);
+
+// let description = country + ' is in ' + continent + ', and its ' + population + ' million people speak ' + language;
+// console.log(description);
+
+// if (population > 33) {
+//     console.log(`${country}'s population is above average.`)
+// } else {
+//     console.log(`${country}'s population is ${33 - population} below average.`)
+// }
+
+// const numNeighbours = prompt('How many neighbour countries does your country have?')
+// if (Number(numNeighbours) === 1) {
+//     console.log('Only 1 border!')
+// } else if (Number(numNeighbours) > 1) {
+//     console.log('More than 1 border.')
+// } else {
+//     console.log('No borders.')
+// }
+
+
+// switch (language) {
+//     case 'chinese':
+//     case 'mandarin':
+//         console.log('MOST number of native speakers!');
+//         break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers.');
+//         break;
+//     case 'english':
+//         console.log('3nd place');
+//         break;
+//     case 'hindi':
+//         console.log('Number 4');
+//         break;
+//     case 'arabic':
+//         console.log('5th mostly spoker language!');
+//         break;
+//     default:
+//         console.log('Great language too!');
+// }
+
+// console.log(`${country}'s population is ${population > 33 ? 'above' : 'below'} average.`)
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# 100DaysOfJavaScript
+# 100 Days of Code - JavaScript
+
+I chose to develop a habit of coding in 2022. The objectives are similar to the [#100DaysOfCode challenge](https://www.100daysofcode.com): to code a minimum an hour every day for the next 100 days. But instead of tweeting the results, I push daily work to this repo. 
+
+Languages to master are Python and JavaScript.
+
+Continuing with the projects from ["The Complete JavaScript Course 2022: From Zero to Expert!"](https://www.udemy.com/course/the-complete-javascript-course/) course on Udemy.
