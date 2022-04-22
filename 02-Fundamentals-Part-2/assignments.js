9c0aecaf113829a94fb005e845d4454dc880b1d4
gizat <gizat.makhanov@gmail.com>
2022-04-22 12:23:48 +0600

    Assignment 1

---
+++ 02-Fundamentals-Part-2/assignments.js
@@ -0,0 +1,13 @@
+'use strict';
+
+// Functions
+
+function describeCountry(country, population, capitalCity) {
+    return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+}
+
+console.log(describeCountry('Kazakhstan', 18.75, 'Nur-Sultan'));
+console.log(describeCountry('Japan', 125.8, 'Tokyo'));
+console.log(describeCountry('Denmark', 5.8, 'Copenhagen'));
+
+// 
