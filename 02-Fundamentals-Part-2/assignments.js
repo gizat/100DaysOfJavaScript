5efe88336135e4d81d6806373db963ded5577ab8
gizat <gizat.makhanov@gmail.com>
2022-04-22 12:40:22 +0600

    Finished assignments

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -20,9 +20,9 @@
 // console.log(percentageOfWorld1(125.8));
 // console.log(percentageOfWorld1(18.75));
 
-// const percentageOfWorld2 = function (population) {
-//     return population / 7900 * 100;
-// }
+const percentageOfWorld2 = function (population) {
+    return population / 7900 * 100;
+}
 
 // console.log(percentageOfWorld2(18.75));
 // console.log(percentageOfWorld2(125.8));
@@ -30,8 +30,19 @@
 
 // Arrow functions
 
-const percentageOfWorld3 = population => population / 7900 * 100;
+// const percentageOfWorld3 = population => population / 7900 * 100;
 
-console.log(percentageOfWorld3(18.75));
-console.log(percentageOfWorld3(125.8));
-console.log(percentageOfWorld3(18.75));
+// console.log(percentageOfWorld3(18.75));
+// console.log(percentageOfWorld3(125.8));
+// console.log(percentageOfWorld3(18.75));
+
+// Functions Calling Other Functions
+
+const describePopulation = function (country, population) {
+    const percentage = percentageOfWorld2(population);
+    return `${country} has ${population} million people, which is about ${percentage}% of the world.`
+}
+
+console.log(describePopulation('Kazakhstan', 18.75));
+console.log(describePopulation('Japan', 125.8));
+console.log(describePopulation('Denmark', 18.75));
