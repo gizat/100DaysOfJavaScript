3363333d183c5b23ce211d720b9b8e444be321df
gizat <gizat.makhanov@gmail.com>
2022-04-22 12:33:27 +0600

    Finished assignments 2-3

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -2,12 +2,36 @@
 
 // Functions
 
-function describeCountry(country, population, capitalCity) {
-    return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
-}
+// function describeCountry(country, population, capitalCity) {
+//     return `${country} has ${population} million people and its capital city is ${capitalCity}.`;
+// }
 
-console.log(describeCountry('Kazakhstan', 18.75, 'Nur-Sultan'));
-console.log(describeCountry('Japan', 125.8, 'Tokyo'));
-console.log(describeCountry('Denmark', 5.8, 'Copenhagen'));
+// console.log(describeCountry('Kazakhstan', 18.75, 'Nur-Sultan'));
+// console.log(describeCountry('Japan', 125.8, 'Tokyo'));
+// console.log(describeCountry('Denmark', 5.8, 'Copenhagen'));
 
-// 
+// Function Declarations vs. Expressions
+
+// function percentageOfWorld1(population) {
+//     return population / 7900 * 100;
+// }
+
+// console.log(percentageOfWorld1(18.75));
+// console.log(percentageOfWorld1(125.8));
+// console.log(percentageOfWorld1(18.75));
+
+// const percentageOfWorld2 = function (population) {
+//     return population / 7900 * 100;
+// }
+
+// console.log(percentageOfWorld2(18.75));
+// console.log(percentageOfWorld2(125.8));
+// console.log(percentageOfWorld2(18.75));
+
+// Arrow functions
+
+const percentageOfWorld3 = population => population / 7900 * 100;
+
+console.log(percentageOfWorld3(18.75));
+console.log(percentageOfWorld3(125.8));
+console.log(percentageOfWorld3(18.75));
