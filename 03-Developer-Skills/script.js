d0907f6694fb3a011fa21fc7d296469af38f2873
gizat <gizat.makhanov@gmail.com>
2022-04-25 09:01:04 +0600

    Finished the challenge

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,2 +1,11 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
+
+const printForecast = function (arr) {
+  let forecastText = '';
+  for (let i = 0; i < arr.length; i++) {
+    forecastText += `${arr[i]}°C in ${i + 1} days ... `;
+  }
+  console.log(forecastText);
+};
+printForecast([17, 21, 23]);
